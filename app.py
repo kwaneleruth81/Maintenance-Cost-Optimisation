9f1ae50a5a98ab043addacfb61457c321bea5dd5
kwaneleruth81 <kwanelesciency@gmail.com>
2024-06-10 04:02:48 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,8 +9,6 @@ st.title('Maintenance Cost Optimization with Random Forest')
 
 # Define the input parameters with initial dummy data
 initial_data = {
-    'Coal usage (tonnes)': 25068.23,
-    'Diesel usage Litres': 6453.15,
     'Reliability %': 33.42,
 }
 
@@ -45,6 +43,9 @@ st.sidebar.number_input('Ave generation (MW)', value=ave_generation, disabled=Tr
 st.sidebar.number_input('Electricity sent out (GWh)', value=electricity_sent_out, disabled=True)
 st.sidebar.number_input('AV BLR efficiency %', value=av_blr_efficiency, disabled=True)
 st.sidebar.number_input('MTBM (Hrs)', value=av_blr_efficiency, disabled=True)
+st.sidebar.number_input('Coal usage (tonnes)', value=25068.23, disabled=False)
+st.sidebar.number_input('Diesel usage Litres', value=6453.15, disabled=False)
+
 
 # Display the input parameters
 #st.sidebar.write('MTBM (Hrs)', 100.88)
